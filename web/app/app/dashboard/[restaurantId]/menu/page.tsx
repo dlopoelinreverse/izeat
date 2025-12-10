e22730281909b01bb7eefc81630695bc41c0f42a
dlopoelinreverse <l.lesaul@gmail.com>
2025-12-10 18:24:15 +0100

    feat: Implement menu creation feature including UI pages, GraphQL mutation, backend base response, and path middleware.

--- web/app/app/dashboard/[restaurantId]/menu/page.tsx
+++ web/app/app/dashboard/[restaurantId]/menu/page.tsx
@@ -1,11 +1,16 @@
-import { getDashboardStatus } from "@/lib/get-dashboard-status";
-import { redirect } from "next/navigation";
+// import { getDashboardStatus } from "@/lib/get-dashboard-status";
+// import { redirect } from "next/navigation";
 
-export default async function MenuPage() {
-  const status = await getDashboardStatus();
+export default async function MenuPage({
+  params,
+}: {
+  params: Promise<{ restaurantId: string }>;
+}) {
+  // const status = await getDashboardStatus();
 
-  if (!status.checks.hasMenu) {
-    redirect(`/app/dashboard/${status.restaurantId}/menu/create`);
-  }
-  return <div>Menu</div>;
+  // if (!status.checks.hasMenu) {
+  //   redirect(`/app/dashboard/${status.restaurantId}/menu/create`);
+  // }
+  const { restaurantId } = await params;
+  return <div>Menu {restaurantId}</div>;
 }
