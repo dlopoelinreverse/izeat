3a8316ff5676d80fdfa20f6b2a5824ed49e0cb4e
dlopoelinreverse <l.lesaul@gmail.com>
2026-03-05 18:27:25 +0100

    debug + cookie parse

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,38 +1,81 @@
 import Session from "./entities/session.entity";
 import User from "./entities/user.entity";
+// import User from "./entities/user.entity";
 import { ContextType } from "./types";
+import { parse } from "cookie";
 
 export async function authChecker({ context }: { context: ContextType }) {
+  // const { req } = context;
+
+  // const cookies = req.headers.cookie;
+  // let sessionToken;
+
+  // if (cookies) {
+  //   const match = cookies.match(/better-auth\.session_token=([^;]+)/);
+  //   if (match) {
+  //     sessionToken = decodeURIComponent(match[1]).split(".")[0];
+  //   }
+  // }
+  // const sessionData = await Session.findOne({
+  //   where: { token: sessionToken },
+  //   relations: ["user"],
+  // });
+
+  // if (!sessionData) {
+  //   console.log("❌ Session non trouvée !");
+  //   return false;
+  // }
+
+  // const now = Date.now();
+
+  // if (sessionData.expiresAt.getTime() < now) {
+  //   console.log("❌ Session expirée !");
+  //   return false;
+  // }
+
+  // const currentUser = await User.findOne({
+  //   where: { id: sessionData.userId },
+  // });
+
+  // if (!currentUser) {
+  //   console.log("❌ Utilisateur non trouvé !");
+  //   return false;
+  // }
+
+  // context.currentUser = currentUser;
+
+  // return true;
+
   const { req } = context;
 
-  const cookies = req.headers.cookie;
-  let sessionToken;
+  const cookies = parse(req.headers.cookie ?? "");
 
-  if (cookies) {
-    const match = cookies.match(/better-auth\.session_token=([^;]+)/);
-    if (match) {
-      sessionToken = decodeURIComponent(match[1]).split(".")[0];
-    }
+  const sessionToken = cookies["better-auth.session_token"]?.split(".")[0];
+
+  if (!sessionToken) {
+    console.log("❌ Aucun session token");
+    return false;
   }
-  const sessionData = await Session.findOne({
+
+  console.log("Session token reçu:", sessionToken);
+
+  const session = await Session.findOne({
     where: { token: sessionToken },
     relations: ["user"],
   });
 
-  if (!sessionData) {
-    console.log("❌ Session non trouvée !");
+  if (!session) {
+    console.log("❌ Session introuvable");
     return false;
   }
 
-  const now = Date.now();
-
-  if (sessionData.expiresAt.getTime() < now) {
-    console.log("❌ Session expirée !");
+  if (session.expiresAt.getTime() < Date.now()) {
+    console.log("❌ Session expirée");
     return false;
   }
 
   const currentUser = await User.findOne({
-    where: { id: sessionData.userId },
+    where: { id: session.userId },
   });
 
   if (!currentUser) {
